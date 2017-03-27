aaa981503eea7fcfe0ecf463cec0d9097405e0bd
Harry Muir <hmuir@pernix-solutions.com>
2017-03-27 09:11:33 -0600

    Base Project

---
+++ Code/luval.vision.api/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace luval.vision.api.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
