35477bfadc1178d7edd1599109955c17cb2f4772
Oscar Marin <oscar@marin.cr>
2017-03-28 13:53:50 -0600

    added a way to convert pdf to image

---
+++ Code/luval.tests/WhenAPDFIsConvertedToImage.cs
@@ -0,0 +1,20 @@
+﻿using luval.vision.core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.tests
+{
+    [TestFixture]
+    public class WhenAPDFIsConvertedToImage
+    {
+        [Test]
+        public void ItShouldDoTheConversionWithoutErrors()
+        {
+            Pdf2Img.ConverToSingleImage(@"C:\Users\Oscar\Desktop\Tmp\sample.pdf", @"C:\Users\Oscar\Desktop\Tmp\sample-img.jpeg");
+        }
+    }
+}
