5f54f37f813a6c47f6531f18fbc644adc55845af
Harry Muir <harry182894@gmail.com>
2017-03-29 15:34:50 -0600

    Finished ticket #7

--- Code/luval.vision.core/Enum.cs
+++ Code/luval.vision.core/Enum.cs
@@ -2,5 +2,4 @@
 public enum DataType { None, Amount, Number, Date, Word, Code }
 
 public enum Direction { None, Top, Down, Left, Right }
-
 public enum EntityType { None, Person, Location, Organization, Event, WorkofArt, ConsumerGood, Amount, Number, Date, Code, Word, Email, Other }
