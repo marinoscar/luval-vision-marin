7b715b205388404ed153df5e7fb9917c4672857d
Harry Muir <harry182894@gmail.com>
2017-04-01 21:46:02 -0600

    Added Azure Blob Storage service, configuration, library

---
+++ Code/luval.vision.bll/OcrBlobStorage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace luval.vision.bll
+{
+    public class OcrBlobStorage
+    {
+        private CloudStorageAccount storage;
+        private CloudBlobClient blobClient;
+        private CloudBlobContainer blobContainer;
+        private CloudBlockBlob blockBlob;
+
+        public OcrBlobStorage()
+        {
+            BlobStorageConfiguration();
+        }
+
+        public void UploadFileBlobStorage(string path)
+        {
+            using (var fileStream = System.IO.File.OpenRead(path))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+
+        private void BlobStorageConfiguration()
+        {
+            storage = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("azure.service.blob.storage.key"));
+            blobClient = storage.CreateCloudBlobClient();
+            blobContainer = blobClient.GetContainerReference("celeris-container");
+            blobContainer.CreateIfNotExists();
+            blobContainer.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+            blockBlob = blobContainer.GetBlockBlobReference("celeris-blob");
+        }
+    }
+}
