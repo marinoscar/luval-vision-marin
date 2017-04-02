f2fbe58d150e21c25101a50227c7070ba2116188
Oscar Marin <oscar@marin.cr>
2017-04-02 09:03:10 -0600

    added tests for the distance algorithm

---
+++ Code/luval.tests/WhenUsingStringUtils.cs
@@ -0,0 +1,23 @@
+﻿using luval.vision.core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.tests
+{
+    [TestFixture]
+    public class WhenUsingStringUtils
+    {
+        [Test]
+        public void DistanceOptionShouldWork()
+        {
+            var res1 = StringUtils.CalculateLevenshteinDistance("total", "tota");
+            var res2 = StringUtils.CalculateLevenshteinDistance("Facebook", "Facebok");
+            var res3 = StringUtils.CalculateLevenshteinDistance("Invoice", "Marin Saenz");
+        }
+
+    }
+}
