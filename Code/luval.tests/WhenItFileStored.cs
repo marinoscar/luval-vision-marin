e38713f63b2ac66bef16bec3a74b95b851235bf6
Harry Muir <harry182894@gmail.com>
2017-04-01 21:46:02 -0600

    Added Azure Blob Storage service, configuration, library

---
+++ Code/luval.tests/WhenItFileStored.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using luval.vision.bll;
+
+namespace luval.tests
+{
+    [TestFixture]
+    class WhenItFileStored
+    {
+        [Test]
+        public void ItShouldStoreFile()
+        {
+            OcrBlobStorage blobStorage = new OcrBlobStorage();
+            blobStorage.UploadFileBlobStorage(@"C:\Users\hmuir\Documents\Pernix\luval-vision\Docs\Sample1.jpg");
+        }
+    }
+}
