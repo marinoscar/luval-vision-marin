fa012f622b2430f396d895bb876a6a07d90e69a6
Harry Muir <harry182894@gmail.com>
2017-04-03 13:35:43 -0600

    Added blob storage services

---
+++ Code/luval.vision.bll/OcrProcess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using luval.vision.core;
+using luval.vision.sink;
+using Newtonsoft.Json;
+using System.IO;
+using System.Web;
+
+namespace luval.vision.bll
+{
+    public class OcrProcess
+    {
+        public OcrProcess()
+        {
+        }
+
+        private OcrProvider GetProvider(bool ms)
+        {
+            return !ms ? new OcrProvider(new GoogleOcrEngine(), new GoogleVisionLoader()) : new OcrProvider(new MicrosoftOcrEngine(), new MicrosoftVisionLoader());
+        }
+
+        public string DoSaveResult(string fileName, ProcessResult processResult)
+        {
+            var data = new FormResult()
+            {
+                FileName = fileName,
+                FileData = File.ReadAllBytes(fileName),
+                Result = processResult
+            };
+            var json = JsonConvert.SerializeObject(data, Formatting.None, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
+            return json;
+        }
+
+        public ProcessResult DoProcess(string fileName)
+        {
+            var jsonData = File.ReadAllText(HttpContext.Current.Server.MapPath("~/App_Data/attribute-mapping.json"));
+            var options = JsonConvert.DeserializeObject<List<AttributeMapping>>(jsonData);
+            var provider = new DocumentProcesor(GetProvider(false), new NlpProvider(new GoogleNlpEngine(), new GoogleNlpLoader()));
+            var result = provider.DoProcess(fileName, options);
+            return result;
+        }
+
+        private void LoadText(OcrResult ocrResult)
+        {
+            var sb = new StringBuilder();
+            foreach (var line in ocrResult.Lines)
+            {
+                sb.AppendLine(line.Text);
+            }
+        }
+
+
+        private List<MappingResult> GetData(OcrResult result)
+        {
+            var jsonData = File.ReadAllText("attribute-mapping.json");
+            var options = JsonConvert.DeserializeObject<List<AttributeMapping>>(jsonData);
+            var navigator = new Navigator(result.Info, result.Lines, options);
+            return navigator.ExtractAttributes();
+        }
+    }
+}
---
+++ Code/luval.vision.entity/BlobUploadModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.vision.entity
+{
+    public class BlobUploadModel
+    {
+        public string FileName { get; set; }
+        public string FileUrl { get; set; }
+        public long FileSizeInBytes { get; set; }
+        public long FileSizeInKb { get { return (long)Math.Ceiling((double)FileSizeInBytes / 1024); } }
+    }
+}
