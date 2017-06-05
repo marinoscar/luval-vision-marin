67c4544aa94e499263f77406527c683189b8636e
Oscar Marin <oscar@marin.cr>
2017-06-04 21:32:08 -0600

    class to handle the working directories of the mapping tool

---
+++ Code/luval.vision.sink/WorkingDir.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.vision.sink
+{
+    public static class WorkingDir
+    {
+        private static DirectoryInfo _imageDir { get; set; }
+        private static DirectoryInfo _processedDir { get; set; }
+        private static DirectoryInfo _resultDir { get; set; }
+        private static DirectoryInfo _skippedDir { get; set; }
+
+        private static void LoadAll()
+        {
+            if (!(_imageDir == null || _processedDir == null || _resultDir == null || _skippedDir == null))
+                return;
+            var dir = ConfigurationManager.AppSettings["workingFolder"];
+            if (string.IsNullOrWhiteSpace(dir))
+            {
+                dir = Environment.CurrentDirectory;
+            }
+            _imageDir = new DirectoryInfo(dir);
+            _processedDir = new DirectoryInfo(_imageDir.FullName + @"\Processed");
+            _resultDir = new DirectoryInfo(_imageDir.FullName + @"\Result");
+            _skippedDir = new DirectoryInfo(_imageDir.FullName + @"\Skipped");
+            CheckAndCreate(_processedDir);
+            CheckAndCreate(_resultDir);
+            CheckAndCreate(_skippedDir);
+
+        }
+        private static void CheckAndCreate(DirectoryInfo dir)
+        {
+            if (!dir.Exists) dir.Create();
+        }
+
+
+        public static DirectoryInfo Image
+        {
+            get
+            {
+                LoadAll();
+                return _imageDir;
+            }
+        }
+
+        public static DirectoryInfo Processed
+        {
+            get
+            {
+                LoadAll();
+                return _processedDir;
+            }
+        }
+
+        public static DirectoryInfo Result
+        {
+            get
+            {
+                LoadAll();
+                return _resultDir;
+            }
+        }
+
+        public static DirectoryInfo Skipped
+        {
+            get
+            {
+                LoadAll();
+                return _skippedDir;
+            }
+        }
+    }
+}
