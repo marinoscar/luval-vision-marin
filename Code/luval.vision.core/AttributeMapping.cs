915d8dd0435f271398ae25b642511c5a94e038e9
Oscar Marin <oscar@marin.cr>
2017-06-04 21:40:08 -0600

    added a new attribute for the mapping of values

--- Code/luval.vision.core/AttributeMapping.cs
+++ Code/luval.vision.core/AttributeMapping.cs
@@ -30,5 +30,7 @@ namespace luval.vision.core
         public bool IsValueLast { get; set; }
         [JsonProperty(PropertyName = "anchorBlackList")]
         public string[] AnchorBlackList { get; set; }
+        [JsonProperty(PropertyName = "notFound")]
+        public bool NotFound { get; set; }
     }
 }
