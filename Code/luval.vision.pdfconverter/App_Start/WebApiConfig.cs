2363629b174c67bca5c775f532d8a6bb594288ac
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-06-20 13:13:17 -0600

    add pdf to image converter stand-alone web api project

---
+++ Code/luval.vision.pdfconverter/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace luval.vision.pdfconverter
+{
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
+    {
+      EnableCrossSiteRequests(config);
+      AddRoutes(config);
+    }
+
+    private static void AddRoutes(HttpConfiguration config)
+    {
+      config.Routes.MapHttpRoute(
+        name: "Default",
+        routeTemplate: "api/{controller}/{action}/{id}",
+        defaults: new { id = RouteParameter.Optional }
+      );
+    }
+
+    private static void EnableCrossSiteRequests(HttpConfiguration config)
+    {
+      var cors = new EnableCorsAttribute(
+        origins: "*",
+        headers: "*",
+        methods: "*");
+      config.EnableCors(cors);
+    }
+  }
+}
---
+++ Code/luval.vision.pdfconverter/Controllers/ImageFromPdfController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Threading.Tasks;
+
+using luval.vision.core;
+
+namespace luval.vision.pdfconverter.Controllers
+{
+  [EnableCors(origins: "*", headers: "*", methods: "*")]
+  public class ImageFromPdfController : ApiController
+  {
+    [HttpPost]
+    public async Task<IHttpActionResult> Post()
+    {
+      byte[] img = null;
+      /* Check if the request is multipart. */
+      if (!Request.Content.IsMimeMultipartContent())
+      {
+        throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+      }
+      try
+      {
+        /* Get the contents of the requests. */
+        var provider = new MultipartMemoryStreamProvider();
+        await Request.Content.ReadAsMultipartAsync(provider);
+        foreach (var file in provider.Contents)
+        {
+          /* Get the pdf as a byte buffer. */
+          var buffer = await file.ReadAsByteArrayAsync();
+          /* Convert the PDF into an Image and assign it to return value. */
+          img = Pdf2Img.ConverToSingleImage(buffer);
+        }
+      } catch ( Exception e )
+      {
+        return BadRequest(e.ToString());
+      }
+      /* If img is null, either there were no images in the request
+       * or there was an error. */
+      if(img == null)
+      {
+        return BadRequest("There was an error converting the PDF");
+      }
+      return Ok(img);
+    }
+  }
+}
