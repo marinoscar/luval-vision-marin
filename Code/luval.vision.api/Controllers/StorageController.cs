eb784e56857debf6afe453cffbbc7266cfbf2983
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-07-11 12:45:22 -0600

    separate statistics generation from storagecontroller

--- Code/luval.vision.api/Controllers/StorageController.cs
+++ Code/luval.vision.api/Controllers/StorageController.cs
@@ -14,6 +14,7 @@ using luval.vision.core;
 using luval.vision.bll;
 using luval.vision.entity;
 using luval.vision.api.Security;
+using luval.vision.api.Statistics;
 
 
 namespace luval.vision.api.Controllers
@@ -35,7 +36,7 @@ namespace luval.vision.api.Controllers
             {
                 IEnumerable<OcrDocument> documents;
                 documents = documentLogic.GetProcessResultByUserId(userId);
-                if (documentCollectionIsNotNull(documents))
+                if (CollectionValidator.IEnumerableCollectionIsNotNull(documents))
                 {
                     return Ok(documents);
                 }
@@ -47,11 +48,6 @@ namespace luval.vision.api.Controllers
             }
         }
 
-        private bool documentCollectionIsNotNull(IEnumerable<OcrDocument> documents)
-        {
-            return !documents.Equals(null);
-        }
-
         [Route("api/v1/Storage/GetStatistics")]
         [HttpGet]
         [BasicAuthentication]
@@ -60,109 +56,10 @@ namespace luval.vision.api.Controllers
             IEnumerable<OcrDocument> documents;
             documents = documentLogic.GetProcessResultByUserId(userId);
 
-            if (documentCollectionIsNotNull(documents) && documents.Count() > 0)
-            {
-                Data returnData = GetStatisticsData(documents);
-                return Ok(returnData);
-            }
-            else
-            {
-                return InternalServerError();
-            }
-        }
+            StatisticsDataGenerator generator = new StatisticsDataGenerator();
+            Data returnData = generator.GenerateDataFromDocuments(documents);
 
-        private Data GetStatisticsData(IEnumerable<OcrDocument> documents)
-        {
-            Data StatisticsData = InitialData();
-            StatisticsData = GetStatisticsDataFromDocuments(StatisticsData, documents);
-            return StatisticsData;
-        }
-
-        private Data InitialData()
-        {
-            return new Data()
-            {
-                cols = new List<Col>(),
-                rows = new List<Row>()
-            };
-        }
-
-        private Data GetStatisticsDataFromDocuments(Data data, IEnumerable<OcrDocument> documents)
-        {
-            data.cols = FillColumns(data.cols);
-            data.rows = FillRowsWithDocumentsData(data.rows, documents);
-            return data;
-        }
-
-        private List<Col> FillColumns(List<Col> cols)
-        {
-            cols.Add(new Col() {
-                         id = "d",
-                         label = "Date",
-                         type = "string"
-                     });
-            cols.Add(new Col() {
-                         id = "n",
-                         label = "Number of Documents",
-                         type = "number"
-                     });
-            return cols;
-        }
-
-        private List<Row> FillRowsWithDocumentsData(List<Row> rows, IEnumerable<OcrDocument> documents)
-        {
-            var dates = new List<string>();
-            var occurrences = new List<int>();
-
-            SetDocumentDatesAndOccurrences(documents, dates, occurrences);
-
-            FillRows(rows, dates, occurrences);
-            return rows;
-        }
-
-        private void SetDocumentDatesAndOccurrences(IEnumerable<OcrDocument> documents, List<string> dates, List<int> occurrences)
-        {
-            foreach (var doc in documents)
-            {
-                var date = GetDateInDoc(doc);
-                var testIndex = dates.FindIndex(x => x == date );
-                if (testIndex != -1)
-                {
-                    occurrences[testIndex] += 1;
-                }
-                else
-                {
-                    dates.Add(date);
-                    /* It appears at least once. */
-                    occurrences.Add(1);
-                }
-            }
-        }
-
-        private void FillRows(List<Row> rows, List<string> dates, List<int> occurrences)
-        {
-            for (int i = 0; i < dates.Count; i++)
-            {
-                var rowContents = new List<C>();
-                rowContents.Add(new C() { v = dates[i] });
-                rowContents.Add(new C() { v = occurrences[i].ToString() });
-                rows.Add(new Row() { c = rowContents });
-            }
-        }
-
-        private string GetDateInDoc(OcrDocument doc)
-        {
-            /* Regular expression matches date in format: yyyy-mm-ddThh:MM:ss.usZ */
-            Regex date_regex = new Regex(@"UtcTimestamp(.*?)\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,7}Z");
-            var rawDate = date_regex.Match(doc.Content).ToString();
-            var formattedDate = GetDateInYearMonthDayFormat(rawDate);
-            return formattedDate;
-        }
-
-        private string GetDateInYearMonthDayFormat(string rawDateString)
-        {
-            /* Date format is: yyyy-mm-dd. */
-            return rawDateString.Substring(15, 10);
+            return Ok(returnData);
         }
     }
 }
