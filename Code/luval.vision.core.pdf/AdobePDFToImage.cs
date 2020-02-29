2639e660e188bc2e254f30cde635befcbfbc1cf7
marinoscar <oscar@marin.cr>
2020-02-29 12:31:41 -0600

    several improvements

---
+++ Code/luval.vision.core.pdf/AdobePDFToImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.vision.core.pdf
+{
+    public class AdobePDFToImage
+    {
+        public AdobePDFToImage()
+        {
+            
+        }
+    }
+}
---
+++ Code/luval.vision.core/resolvers/ConstantResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.vision.core.resolvers
+{
+    public class ConstantResolver : IStringResolver
+    {
+        public string Code => "constant";
+
+        public string GetValue(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ResolverMatch> GetValues(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMatch(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
