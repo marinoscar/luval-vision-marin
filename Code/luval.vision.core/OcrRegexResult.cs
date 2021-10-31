d5810b17c5465be13df656dddcd1dcaa3f7fe162
Oscar Marin <oscar@marin.cr>
2021-10-31 11:01:31 -0500

    Create OcrRegexResult.cs
    added new class for better search results

---
+++ Code/luval.vision.core/OcrRegexResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace luval.vision.core
+{
+    public class OcrRegexResult
+    {
+        public OcrElement Element { get; set; }
+        public string RegExPattern { get; set; }
+        public Match Match { get; set; }
+    }
+}
