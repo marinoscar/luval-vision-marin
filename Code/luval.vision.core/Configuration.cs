8bcaba0d28296b9bad42ae3a71d46ada89a8ea46
Oscar Marin <oscar@marin.cr>
2021-11-05 20:17:41 -0500

    added new configuration class

---
+++ Code/luval.vision.core/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace luval.vision.core
+{
+    public class Configuration
+    {
+        public Configuration()
+        {
+            Mappings = new List<AttributeMapping>();
+        }
+        public List<AttributeMapping> Mappings { get; set; }
+    }
+}
