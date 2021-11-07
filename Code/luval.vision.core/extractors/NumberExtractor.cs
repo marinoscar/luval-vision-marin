f9106a5db07c58de9aa0542ab4eacd6f21c88d77
Oscar Marin <oscar@marin.cr>
2021-11-07 17:02:01 -0600

    Improved the number extractor

--- Code/luval.vision.core/extractors/NumberExtractor.cs
+++ Code/luval.vision.core/extractors/NumberExtractor.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.Recognizers.Text.Number;
+﻿using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.Number;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace luval.vision.core.extractors
 {
     public class NumberExtractor : MicrosoftRecognizerExtractor
     {
-        public NumberExtractor() : base((query, culture) => NumberRecognizer.RecognizeNumber(query, culture))
+        public NumberExtractor() : base((query, culture) => DoNumber(query, culture))
         {
+        }
+
+        private static List<ModelResult> DoNumber(string query, string culture)
+        {
+            return NumberRecognizer.RecognizeNumber(query, culture).Where(i => i.Text.IsNumericValue()).ToList();
         }
     }
 }
