760f1f9440b54e015a141c4a0d8aa30a5805483e
Oscar Marin <oscar@marin.cr>
2021-11-07 18:33:20 -0600

    fix issue with method

--- Code/luval.vision.core/StringUtils.cs
+++ Code/luval.vision.core/StringUtils.cs
@@ -46,6 +46,7 @@ namespace luval.vision.core
 
         public static string TrimEx(this string s)
         {
+            if (string.IsNullOrWhiteSpace(s)) return s;
             return s.Trim().Trim('_');
         }
     }
