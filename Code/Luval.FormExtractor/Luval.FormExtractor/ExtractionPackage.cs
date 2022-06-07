062bdbb802e3399fdbe3fc39abd06266d881652d
Oscar Marin <oscar@marin.cr>
2022-06-07 10:37:07 -0500

    Added class to handle the data

--- Code/Luval.FormExtractor/Luval.FormExtractor/ExtractionPackage.cs
+++ Code/Luval.FormExtractor/Luval.FormExtractor/ExtractionPackage.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Luval.FormExtractor
 {
-    public class ExtractionResult
+    public class ExtractionPackage
     {
 
         public DataTable Table { get; set; }
