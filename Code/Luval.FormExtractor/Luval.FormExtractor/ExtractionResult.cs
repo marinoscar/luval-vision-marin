3791527c62dc4fc5a062a067983f40744b121799
Oscar Marin <oscar@marin.cr>
2022-06-07 10:32:30 -0500

    Added a new class to handle the datatable and json result

---
+++ Code/Luval.FormExtractor/Luval.FormExtractor/ExtractionResult.cs
@@ -0,0 +1,17 @@
+﻿using luval.vision.core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.FormExtractor
+{
+    public class ExtractionResult
+    {
+
+        public DataTable Table { get; set; }
+        public OcrResult OcrResult { get; set; }
+    }
+}
